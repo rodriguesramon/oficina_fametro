15489d10336d815c95e0f3a38832f5569c5efcc0
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2021-11-18 21:28:40 -0400

    Programa que pede o salario

---
+++ Programa_04/Program.cs
@@ -0,0 +1,18 @@
+﻿string nome;
+int idade;
+double pretensao_salarial;
+
+Console.WriteLine("Informe o seu nome: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a idade: ");
+idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Pretensão Salarial: ");
+pretensao_salarial = double.Parse(Console.ReadLine());
+
+// var texto_final = "Nome: " + nome + ", Idade: " + idade + ", Pretensão Salarial: " + pretensao_salarial;
+var texto_final = $"Nome: {nome}, Idade: {idade}, Pretensão: {pretensao_salarial}";
+
+Console.WriteLine(texto_final);
+
